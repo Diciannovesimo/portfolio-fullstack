cd585577b2df816da9a1a28d1d5554cf56f820b4
Diciannovesimo <williamcolucci1998@gmail.com>
2022-04-23 16:44:18 +0200

    Animazione Navcard, uso migliore css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {motion} from "framer-motion";
-import {useState} from "react";
+import { useState } from "react";
+import { motion,} from "framer-motion";
 
 function App() {
 
@@ -7,18 +7,26 @@ function App() {
 
   return (
     <div className="App">
-      <motion.div transition={{layout: {duration: 1, type: "spring"}}} Layout onClick={() => setIsOpen(!isOpen)} className="card" style={{borderRadius: "2rem", boxShadow: '0px 10px 30px rgba(0,0,0, 0.5)'}}>
-        <motion.h2 Layout="position">Framer Motion 🤠</motion.h2>
-        {isOpen &&(
-          <motion.div>
+      <motion.div
+        layout transition={{ layout: { duration: 0.75, type: "spring" } }}
+        onClick={() => setIsOpen(!isOpen)}
+        className="card">
+
+        <motion.h2 layout="position">Framer Motion 🤠</motion.h2>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            className="expand">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Adipisci quia corporis obcaecati eum rem ipsum aliquam perspiciatis culpa doloribus. 
-              Quam, quos tenetur? Doloribus eligendi sint reiciendis illum error quisquam. 
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Adipisci quia corporis obcaecati eum rem ipsum aliquam perspiciatis culpa doloribus.
+              Quam, quos tenetur? Doloribus eligendi sint reiciendis illum error quisquam.
               Ipsum?
             </p>
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Inventore, pariatur.
             </p>
           </motion.div>
