931d788393b9983ff33d4ceccf64ee67223ea91f
Diciannovesimo <williamcolucci1998@gmail.com>
2022-04-24 21:14:41 +0200

    New Function Cardanimated, New Folder Components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import { useState } from "react";
-import { motion,} from "framer-motion";
+import Cardanimated from "./components/Cardanimated";
 
 function App() {
-
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <div className="App">
-      <motion.div
-        layout transition={{ layout: { duration: 0.75, type: "spring" } }}
-        onClick={() => setIsOpen(!isOpen)}
-        className="card">
 
-        <motion.h2 layout="position">Framer Motion 🤠</motion.h2>
-        {isOpen && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className="expand">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Adipisci quia corporis obcaecati eum rem ipsum aliquam perspiciatis culpa doloribus.
-              Quam, quos tenetur? Doloribus eligendi sint reiciendis illum error quisquam.
-              Ipsum?
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Inventore, pariatur.
-            </p>
-          </motion.div>
-        )}
-      </motion.div>
+    <Cardanimated/>
     </div>
   );
 }
