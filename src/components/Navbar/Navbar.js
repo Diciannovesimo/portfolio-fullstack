16fafcde3c378caf7e8394f3bdf34c31719cf5d0
Diciannovesimo <williamcolucci1998@gmail.com>
2022-04-26 20:19:20 +0200

    Nav Mobile, Nav css improved

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ function Navbar() {
 
     return (
         <motion.div className="navbar">
-            <motion.h4 style={{ fontFamily: 'Poppins', textTransform: 'uppercase', letterSpacing: '4px', fontSize: '22px', color: '#5E4E3A'}}> The Nav </motion.h4>
-            <motion.ul style={{ display: 'flex', justifyContent: 'space-around', width: '35%', /*background: 'red'*/ }}>
+            <motion.h4> The Nav </motion.h4>
+            <motion.ul>
                 <motion.li><motion.a href="#">Home</motion.a></motion.li>
                 <motion.li><motion.a href="#">About</motion.a></motion.li>
                 <motion.li><motion.a href="#">Work</motion.a></motion.li>
