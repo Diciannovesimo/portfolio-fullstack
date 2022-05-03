01c7e401bf9d3afca20c08bcb5226996186c100f
Diciannovesimo <williamcolucci1998@gmail.com>
2022-05-04 00:21:41 +0200

    Nav Modified, button, lateral menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,13 +5,24 @@ import React, { useState } from "react";
 
 function Navbar() {
     const [slider, setSlider] = useState(false);
+    const burger = document.querySelector('.burger');
+
+    
 
     const slide = () => {
         setSlider(!slider);
-      }
-    
+        burger.classList.toggle('toggle');
+    }
+
+
     return (
         <motion.div className="navbar">
+            <motion.div className="burger"
+                onClick={slide}>
+                <motion.h1 className="line1"/>
+                <motion.h1 className="line2"/>
+                <motion.h1 className="line3"/>
+            </motion.div>
             <motion.h4> The Nav </motion.h4>
             <motion.ul className={slider ? "slider-on" : "slider-off"}>
                 <motion.li><motion.a href="#">Home</motion.a></motion.li>
@@ -19,14 +30,7 @@ function Navbar() {
                 <motion.li><motion.a href="#">Work</motion.a></motion.li>
                 <motion.li><motion.a href="#">Projects</motion.a></motion.li>
             </motion.ul>
-            <motion.div className="burger"
-            onClick={slide}>
-                <motion.h1 />
-                <motion.h1 />
-                <motion.h1 />
-            </motion.div>
         </motion.div>
-
     );
 
 }
