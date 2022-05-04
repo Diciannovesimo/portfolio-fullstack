d008f3811f1df449ecab89676a6bade83e91f3e7
Diciannovesimo <williamcolucci1998@gmail.com>
2022-05-04 13:37:19 +0200

    burger circle, navmodified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,26 +5,21 @@ import React, { useState } from "react";
 
 function Navbar() {
     const [slider, setSlider] = useState(false);
-    const burger = document.querySelector('.burger');
-
     
 
     const slide = () => {
         setSlider(!slider);
-        burger.classList.toggle('toggle');
     }
-
 
     return (
         <motion.div className="navbar">
             <motion.div className="burger"
                 onClick={slide}>
-                <motion.h1 className="line1"/>
-                <motion.h1 className="line2"/>
-                <motion.h1 className="line3"/>
+                <motion.h1 className={slider ? "toggle1" : "line1"}/>
+                <motion.h1 className={slider ? "toggle2" : "line2"}/>
+                <motion.h1 className={slider ? "toggle3" : "line3"}/>
             </motion.div>
-            <motion.h4> The Nav </motion.h4>
-            <motion.ul className={slider ? "slider-on" : "slider-off"}>
+            <motion.ul className={slider ? "slider-off" : "slider-on"}>
                 <motion.li><motion.a href="#">Home</motion.a></motion.li>
                 <motion.li><motion.a href="#">About</motion.a></motion.li>
                 <motion.li><motion.a href="#">Work</motion.a></motion.li>
